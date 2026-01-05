94311fd9770b8798103167fc6597359354179d01
Serkyo <serkyo-dev@proton.me>
2026-01-05 16:02:10 +0100

    Implémentation du protocole Runnable pour faire des processus parallèles

--- src/main/java/com/laverie/AppareilIOT.java
+++ src/main/java/com/laverie/AppareilIOT.java
@@ -2,13 +2,11 @@ package com.laverie;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
-public abstract class AppareilIOT {
+public abstract class AppareilIOT implements Runnable {
     public static String HOST;
 
-    public static void chargementEnv() {
-        Dotenv dotenv = Dotenv.load();
-
-        HOST = dotenv.get("HOST");
+    public static void chargementEnv(Dotenv instanceDotenv) {
+        HOST = instanceDotenv.get("HOST");
     }
 
     public void recuperation() {
