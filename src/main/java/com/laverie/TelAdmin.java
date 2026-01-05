85dc057d8292c9b78118f940977387b542fbd1a2
Arthur Peyre <peyre.arthur@outlook.fr>
2026-01-05 15:43:36 +0100

    Création de la classe TelAdmin

---
+++ src/main/java/com/laverie/TelAdmin.java
@@ -0,0 +1,24 @@
+package com.laverie;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+/**
+ *
+ * @author arthu
+ */
+public class TelAdmin {
+
+    public TelAdmin() {
+        // ...
+
+        start();
+    }
+
+    public void start() {
+
+    }
+
+}
