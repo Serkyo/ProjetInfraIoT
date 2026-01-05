0f3f63e9fedc1fddf571d95b0aaa586f0808b5a5
Arthur Peyre <peyre.arthur@outlook.fr>
2026-01-05 16:23:42 +0100

    Update NOMBRE_D_APPAREILS in .env and enhance TelAdmin class to receive messages from RabbitMQ

--- src/main/java/com/laverie/Main.java
+++ src/main/java/com/laverie/Main.java
@@ -1,9 +1,9 @@
 package com.laverie;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,5 +11,6 @@ public class Main {
         AppareilIOT.chargementEnv(dotenv);
 
         ExecutorService executorService = Executors.newFixedThreadPool(Integer.parseInt(dotenv.get("NOMBRE_D_APPAREILS")));
+        executorService.execute(new TelAdmin());
     }
 }
--- src/main/java/com/laverie/TelAdmin.java
+++ src/main/java/com/laverie/TelAdmin.java
@@ -1,19 +1,49 @@
 package com.laverie;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+
 /**
  *
  * @author arthu
  */
 public class TelAdmin extends AppareilIOT {
 
-    public TelAdmin() {
-        // ...
+    private static final String EXCHANGE_NAME = "q_admin";
 
+    public TelAdmin() {}
+
+    public void receiveData() throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(AppareilIOT.HOST);
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, "topic");
+        String queueName = channel.queueDeclare().getQueue();
+        channel.queueBind(queueName, EXCHANGE_NAME, "laverie.*");
+
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });
     }
+
 
     @Override
     public void run() {        
         System.out.println("Hello world !");
+        try {
+            receiveData();
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
     }
 
 }
