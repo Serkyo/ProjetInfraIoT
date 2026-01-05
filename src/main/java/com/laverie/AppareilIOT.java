7870faca60d428a15fa01dcee10e311f83cd2017
Serkyo <serkyo-dev@proton.me>
2026-01-05 16:31:58 +0100

    Ajout de l'exchange name en .env

--- src/main/java/com/laverie/AppareilIOT.java
+++ src/main/java/com/laverie/AppareilIOT.java
@@ -4,12 +4,10 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 public abstract class AppareilIOT implements Runnable {
     public static String HOST;
+    public static String EXCHANGE_NAME;
 
     public static void chargementEnv(Dotenv instanceDotenv) {
         HOST = instanceDotenv.get("HOST");
-    }
-
-    public void recuperation() {
-
+        EXCHANGE_NAME = instanceDotenv.get("EXCHANGE_NAME");
     }
 }
