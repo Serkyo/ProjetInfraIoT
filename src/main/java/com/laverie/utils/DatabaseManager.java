2c554b8898a4c1b9050a550cce6e0f309d8034f7
Serkyo <serkyo-dev@proton.me>
2026-01-06 14:59:15 +0100

    Fixed wrong type for id_machine

--- src/main/java/com/laverie/utils/DatabaseManager.java
+++ src/main/java/com/laverie/utils/DatabaseManager.java
@@ -12,13 +12,13 @@ public class DatabaseManager {
     private static final String PASSWORD = System.getenv("POSTGRES_PASSWORD");
     private static final String URL = "jdbc:postgresql://db:5432/" + DB_NAME;
 
-    public static void insererHistoriqueMachine(int idMachine, Cycles cycle, int duree, float consoElec, float consoEau, Date dateDebut, Date dateFin) {
+    public static void insererHistoriqueMachine(String idMachine, Cycles cycle, int duree, float consoElec, float consoEau, Date dateDebut, Date dateFin) {
         String sql = "INSERT INTO historique_machines (id_machine, type_cycle, duree, conso_elec, conso_eau, date_debut, date_fin) VALUES (?, ?, ?, ?, ?, ?, ?)";
 
         try {
             Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
             PreparedStatement stmt = connection.prepareStatement(sql);
-            stmt.setInt(1, idMachine);
+            stmt.setString(1, idMachine);
             stmt.setString(2, cycle.name());
             stmt.setInt(3, duree);
             stmt.setFloat(4, consoElec);
