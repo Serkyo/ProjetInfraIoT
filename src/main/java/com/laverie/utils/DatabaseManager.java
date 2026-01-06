ba48d7c8d9da5e016e631e46ceca6846864f0f5a
Serkyo <serkyo-dev@proton.me>
2026-01-06 14:49:44 +0100

    Added tables and a manager to the db

---
+++ src/main/java/com/laverie/utils/DatabaseManager.java
@@ -0,0 +1,35 @@
+package com.laverie.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Date;
+
+public class DatabaseManager {
+    private static final String DB_NAME = System.getenv("POSTGRES_NAME");
+    private static final String USER = System.getenv("POSTGRES_USER");
+    private static final String PASSWORD = System.getenv("POSTGRES_PASSWORD");
+    private static final String URL = "jdbc:postgresql://db:5432/" + DB_NAME;
+
+    public static void insererHistoriqueMachine(int idMachine, Cycles cycle, int duree, float consoElec, float consoEau, Date dateDebut, Date dateFin) {
+        String sql = "INSERT INTO historique_machines (id_machine, type_cycle, duree, conso_elec, conso_eau, date_debut, date_fin) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+        try {
+            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, idMachine);
+            stmt.setString(2, cycle.name());
+            stmt.setInt(3, duree);
+            stmt.setFloat(4, consoElec);
+            stmt.setFloat(5, consoEau);
+            stmt.setDate(6, (java.sql.Date) dateDebut);
+            stmt.setDate(7, (java.sql.Date) dateFin);
+
+            stmt.executeUpdate();
+            System.out.println("New row inserted into the database");
+        } catch (SQLException e) {
+            System.err.println("An error occured while inserting into the database : " + e.getMessage());
+        }
+    }
+}
--- start_project.sh
+++ start_project.sh
@@ -1,3 +1,4 @@
 mvn clean package
 docker compose up --build -d
+docker compose logs -f
 # docker-compose --env-file .env up -d --build
