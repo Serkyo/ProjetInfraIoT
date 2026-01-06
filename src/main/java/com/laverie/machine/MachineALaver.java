8a0f2f479345891d8e2f983b02bff9769ba68302
Arthur Peyre <peyre.arthur@outlook.fr>
2026-01-06 10:15:07 +0100

    Feat: Receive Func pour Machine à laver

--- src/main/java/com/laverie/machine/MachineALaver.java
+++ src/main/java/com/laverie/machine/MachineALaver.java
@@ -6,14 +6,52 @@ import com.laverie.AppareilIOT;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
 
 public class MachineALaver extends AppareilIOT {
+    public String id;
+
     public static void main(String[] args) {
         System.out.println("DEMARRAGE");
         MachineALaver machineALaver = new MachineALaver();
         System.out.println(HOST);
         System.out.println(EXCHANGE_NAME);
+        machineALaver.receive();
         machineALaver.emmeteur();
+    }
+
+    private void receive() {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(AppareilIOT.HOST);
+        try {
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+    
+            channel.exchangeDeclare(AppareilIOT.EXCHANGE_NAME, "topic");
+            String queueName = channel.queueDeclare().getQueue();
+            channel.queueBind(queueName, AppareilIOT.EXCHANGE_NAME, "laverie.machine." + id +"");
+    
+            // System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+    
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String routingKey = delivery.getEnvelope().getRoutingKey();
+                String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+
+                // Exemple de vérification
+                if (routingKey.equals("laverie.machine." + id + ".toggle")) {
+                    System.out.println(" [x] Message accepté");
+                    System.out.println("     RoutingKey : " + routingKey);
+                    System.out.println("     Message    : " + message);
+
+                    // traitement du message ici
+                } else {
+                    System.out.println(" [!] Message ignoré (routingKey = " + routingKey + ")");
+                }
+            };
+            channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private void emmeteur() {
@@ -25,8 +63,8 @@ public class MachineALaver extends AppareilIOT {
             newChannel.exchangeDeclare(EXCHANGE_NAME, "topic");
 
             while (!Thread.currentThread().isInterrupted()) {
-                System.out.println("Sending one message from machine " + uuid);
-                String routingKey = "laverie.machine." + uuid + ".status";
+                System.out.println("Sending one message from machine " + id);
+                String routingKey = "laverie.machine." + id + ".status";
                 double nombre = Math.random();
                 newChannel.basicPublish(EXCHANGE_NAME, routingKey, null, String.valueOf(nombre > 0.5).getBytes(StandardCharsets.UTF_8));
                 Thread.sleep(5000);
